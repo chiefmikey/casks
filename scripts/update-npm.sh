71cfe59e7f6bc25296adf2acb99d42de3ffb970e
chiefmikey <wolfemikl@gmail.com>
2022-07-18 08:46:00 -0600

    Update install from silent to quiet

--- scripts/update-npm.sh
+++ scripts/update-npm.sh
@@ -4,7 +4,7 @@ echo "+ npm global update"
 for package in $(npm ls -g --json | jq '.dependencies | keys' | awk -F'"' '{print $2}'); do
   export v1=$(${package} --version)
   if [ $(npm view ${package} version) != ${v1} ]; then
-     npm i -gs ${package}
+     npm i -gq ${package}
      export v2=$(${package} --version)
      echo "  ${package} (${v1} -> ${v2})"
   else
