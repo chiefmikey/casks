65c041eb0527672b46e976fe607c93924576d5a2
Mikl Wolfe <wolfemikl@gmail.com>
2022-07-18 08:32:26 -0600

    Update commit-all progress

--- scripts/commit-apps.sh
+++ scripts/commit-apps.sh
@@ -1,6 +1,59 @@
-#!/bin/zsh -v
+#!/bin/zsh
 
-set -x
+export ROOT=${1}
+export x=${2}
+export MESSAGE=${3}
+export METHOD=${4}
+
+if [ -z ${ROOT} ]; then
+  echo "+ Input Root:"
+  read ROOT
+elif [ -n ${ROOT} ]; then
+  if [ -d ${ROOT} ]; then
+    echo "+ Root: ${ROOT}"
+  else
+    echo "+ 
+else
+  echo "+ Method: Unknown"
+  exit 1
+done
+
+
+
+if [ -z ${METHOD} ] || [ ${METHOD} = "rebase" ]; then
+  echo "+ Method: Rebase"
+  export METHOD="rebase"
+elif [ ${METHOD} = "merge" ]; then
+  echo "+ Method: Merge"
+else
+  echo "+ Method: Unknown"
+exit 1
+
+if [ -z ${METHOD} ] || [ ${METHOD} = "rebase" ]; then
+  echo "+ Method: Rebase"
+  export METHOD="rebase"
+elif [ ${METHOD} = "merge" ]; then
+  echo "+ Method: Merge"
+else
+  echo "+ Method: Unknown"
+exit 1
+
+if [ -z ${METHOD} ] || [ ${METHOD} = "rebase" ]; then
+  echo "+ Method: Rebase"
+  export METHOD="rebase"
+elif [ ${METHOD} = "merge" ]; then
+  echo "+ Method: Merge"
+else
+  echo "+ Method: Unknown"
+  exit 1
+fi
+
+if [ ${METHOD} = "rebase" ]; then
+  find ${ROOT} -type d -name ".git" -exec ${ROOT}/extra/scripts/rebase-commit-all.sh {} \;
+elif [ ${METHOD} = "merge" ]; then
+  find ${ROOT} -type d -name ".git" -exec ${ROOT}/extra/scripts/merge-commit-all.sh {} \;
+fi
+
 export PATH=.github
 export MESSAGE='Update dependabot workflow'
 cd ~/dropbox/dev/apps/auth-server && git pull --rebase=false --autostash --no-edit && git add ${PATH} && git commit ${PATH} -m ${MESSAGE} && git push
