afdeb23c12279d8d017a695a5b7ce5a90b9618e9
Mikl Wolfe <wolfemikl@gmail.com>
2022-07-18 08:15:41 -0600

    Update variable capitalization

--- scripts/sync-apps.sh
+++ scripts/sync-apps.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 
-export root=${1}
-export method=${2}
+export ROOT=${1}
+export METHOD=${2}
 
-if [ -z ${method} ] || [ ${method} = "rebase" ]; then
+if [ -z ${METHOD} ] || [ ${METHOD} = "rebase" ]; then
   echo "+ Method: Rebase"
-  export method="rebase"
-elif [ ${method} = "merge" ]; then
+  export METHOD="rebase"
+elif [ ${METHOD} = "merge" ]; then
   echo "+ Method: Merge"
 else
   echo "+ Method: Unknown"
   exit 1
 fi
 
-if [ ${method} = "rebase" ]; then
-  find ${root} -type d -name ".git" -exec ${root}/extra/scripts/rebase-all.sh {} \;
-elif [ ${method} = "merge" ]; then
-  find ${root} -type d -name ".git" -exec ${root}/extra/scripts/merge-all.sh {} \;
+if [ ${METHOD} = "rebase" ]; then
+  find ${ROOT} -type d -name ".git" -exec ${ROOT}/extra/scripts/rebase-all.sh {} \;
+elif [ ${METHOD} = "merge" ]; then
+  find ${ROOT} -type d -name ".git" -exec ${ROOT}/extra/scripts/merge-all.sh {} \;
 fi
