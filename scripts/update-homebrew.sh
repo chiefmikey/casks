e97721bb92838fc80aa7275da7d584fd3efd0f92
chiefmikey <wolfemikl@gmail.com>
2022-07-21 12:54:35 -0600

    Include greedy flag with brew upgrade

--- scripts/update-homebrew.sh
+++ scripts/update-homebrew.sh
@@ -2,6 +2,6 @@
 
 brew autoupdate delete
 echo "+ brew update" && brew update
-echo "+ brew upgrade" && brew upgrade
+echo "+ brew upgrade" && brew upgrade --greedy
 echo "+ brew cleanup" && brew cleanup
 brew autoupdate start 21600 --upgrade --cleanup --greedy --immediate
